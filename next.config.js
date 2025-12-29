3c20f6349995d910c252836c51488f46e84c1d3a
jaimeveg <jvegascda.93@gmail.com>
2025-12-29 11:48:25 +0100

    Cambio mínimo: Comentarios de recursos externos

--- next.config.js
+++ next.config.js
@@ -12,4 +12,7 @@ const nextConfig = {
   },
 };
 
+// [PokeAPI: Máster para obtención de información sobre Pokémon, stats, sprites e imágenenes, movimientos y habilidades, entre otras]
+// [Duiker101: Recursos de imagen SVG para componer visuales de los tipos pokémon]
+
 export default nextConfig;
